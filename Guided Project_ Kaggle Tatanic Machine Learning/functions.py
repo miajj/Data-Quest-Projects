f21d9aebca114034ab109825ac1c935dac295549
mia <yueyaer246@gmail.com>
2018-02-01 15:44:32 +1030

    add kaggle

---
+++ Guided Project_ Kaggle Tatanic Machine Learning/functions.py
@@ -0,0 +1,66 @@
+def process_missing(df):
+    """Handle various missing values from the data set,
+    refill the missing values with mean. 
+    """
+    #df["Fare"] = df["Fare"].fillna(train["Fare"].mean())
+    df["Embarked"] = df["Embarked"].fillna("S")
+    df["Age"] = df["Age"].fillna(-0.5)
+    return df
+
+def process_age(df):
+    """Process the Age column into pre-defined 'bins'/category. 
+    """
+    cut_points = [-1,0,5,12,18,35,60,100]
+    label_names = ["Missing","Infant","Child","Teenager","Young Adult","Adult","Senior"]
+    df["Age_categories"] = pd.cut(df["Age"],cut_points,labels=label_names)
+    return df
+
+def process_fare(df):
+    """Process the Fare column into pre-defined 'bins' 
+    """
+    cut_points = [-1,12,50,100,1000]
+    label_names = ["0-12","12-50","50-100","100+"]
+    df["Fare_categories"] = pd.cut(df["Fare"],cut_points,labels=label_names)
+    return df
+
+def process_cabin(df):
+    """Process the Cabin column into pre-defined 'bins' 
+    """
+    df["Cabin_type"] = df["Cabin"].str[0]
+    df["Cabin_type"] = df["Cabin_type"].fillna("Unknown")
+    #df = df.drop('Cabin',axis=1)
+    return df
+
+def process_titles(df):
+    """Extract and categorize the title from the name column 
+    """
+    titles = {
+        "Mr" :         "Mr",
+        "Mme":         "Mrs",
+        "Ms":          "Mrs",
+        "Mrs" :        "Mrs",
+        "Master" :     "Master",
+        "Mlle":        "Miss",
+        "Miss" :       "Miss",
+        "Capt":        "Officer",
+        "Col":         "Officer",
+        "Major":       "Officer",
+        "Dr":          "Officer",
+        "Rev":         "Officer",
+        "Jonkheer":    "Royalty",
+        "Don":         "Royalty",
+        "Sir" :        "Royalty",
+        "Countess":    "Royalty",
+        "Dona":        "Royalty",
+        "Lady" :       "Royalty"
+    }
+    extracted_titles = df["Name"].str.extract(' ([A-Za-z]+)\.',expand=False)
+    df["Title"] = extracted_titles.map(titles)
+    return df
+
+def create_dummies(df,column_name):
+    """Create Dummy Columns (One Hot Encoding) from a single Column
+    """
+    dummies = pd.get_dummies(df[column_name],prefix=column_name)
+    df = pd.concat([df,dummies],axis=1)
+    return df
